bdf22471379b5a166458ec281e774c3e6f897cc1
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2025-05-21 11:37:11 +0530

    search page minor change

--- client/src/components/Seachbar.jsx
+++ client/src/components/Seachbar.jsx
@@ -15,7 +15,7 @@ const Seachbar = () => {
             setVisible(false);
         }
     },[location]);
-  return showSearch && visible ?  (
+  return showSearch &&   (
     <div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:1/2'>
             <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search'/>
@@ -23,7 +23,7 @@ const Seachbar = () => {
         </div>
         <img onClick={() => setShowSearch(false) } src={assets.cross_icon} className='inline w-3 cursor-pointer' alt="" />
     </div>
-  ) : null
+  ) 
 }
 
 export default Seachbar
